d5f8a4904585ff8e52355731d31ccc0c68d60d9f
Daniel Serrano <danielserrano.contacto@gmail.com>
2023-01-14 21:33:38 +0100

    fix eslit setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,32 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: [
-    "@remix-run/eslint-config", 
-    "@remix-run/eslint-config/node",
+  "env": {
+    "browser": true,
+    "node": true,
+    "es2021": true,
+  },
+  "extends": [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended"
   ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+        "jsx": true,
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module",
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "settings": {
+    "react": {
+      "version": "detect",
+    },
+  },
   rules: {
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/consistent-type-imports": "off"
